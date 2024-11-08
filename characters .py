c5a7503a0428eb18c9a49b11307800dd366e9842
RafaelMartins <rafaelsantosmartins.dev@gmail.com>
2024-11-08 12:05:58 +0000

    Add files via upload

--- characters .py
+++ characters .py
@@ -1,22 +1,31 @@
 characters = []
-max_ataque = -1
-max_defesa = -1
-character_max_ataque = ""
-character_max_defesa = ""
+count = 3
 
-for i in range(3):
-    nome = str(input(""))
-    ataque = (int(input("")))
-    defesa = (int(input("")))
-    characters.append([nome,(ataque,defesa)])
+for i in range(count):
+    name = input("Nome do personagem: ")
+    attack = int(input("Ataque do personagem: "))
+    defense = int(input("Defesa do personagem: "))
+    character = [name, attack, defense]  
+    characters.append(character)
+
+print("Lista de personagens:", characters)
+
+
+max_attack = -1
+max_defense = -1
+max_attack_character = None
+max_defense_character = None
+
+
+for character in characters:
+    if character[1] > max_attack:  
+        max_attack = character[1]
+        max_attack_character = character
     
-    if ataque > max_ataque:
-        max_ataque = ataque
-        character_max_ataque = nome
-    if defesa > max_defesa:
-        max_defesa = defesa
-        character_max_defesa = nome
-    
-    print(characters)
-    print("Ataque: ", max_ataque, ataque)
-    print("Defesa: ", max_defesa, defesa)
+    if character[2] > max_defense: 
+        max_defense = character[2]
+        max_defense_character = character
+
+
+print("Ataque: " + str(max_attack_character[0]) + " " + str(max_attack_character[1]))
+print("Defesa: " + str(max_defense_character[0]) + " " + str(max_defense_character[2]))
